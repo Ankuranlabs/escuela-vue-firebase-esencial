3dcd25926ea0cedc2c13ae8ce70b677406822a52
ejimenez123 <caribestic123@gmail.com>
2021-09-05 17:58:31 -0400

    chore:  Install Vue Toast Notification

--- vue3/vue3-firebase-esencial/src/main.js
+++ vue3/vue3-firebase-esencial/src/main.js
@@ -4,12 +4,14 @@ import router from './router'
 import store from './store'
 //import "tailwindcss/tailwind.css";
 import "./assets/css/app.css"
-
+import VueToast from "vue-toast-notification";
+//import "vue-toast-notification/dist/theme-default.css";
+import 'vue-toast-notification/dist/theme-sugar.css';
 
 import {db} from "./firebase";
 console.log(db);
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(VueToast).mount('#app')
 //import VueToast from "vue-toast-notification";
 //import "vue-toast-notification/dist/theme-default.css";
 
